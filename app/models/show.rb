3c3fd5e3a87298b959ba49b1636933b15c4f0f9d
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-18 18:06:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.select.maximum(:rating).limit(1)
+    Show.select(:name).maximum(:rating).limit(1)
   end
 
   def self.lowest_rating
