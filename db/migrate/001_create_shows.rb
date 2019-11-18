60e5b698cc1a2219ae8a369dd6f005202cfae6c6
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-18 17:39:59 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,7 @@
 # In this file, write the migration code to create a shows table.
 # The table should have name, network, day, and rating columns.
 # name, network, and day have a datatype of string, and rating has a datatype of integer.
-class CreateShows < ActiveRecord::Migration
+class CreateShows < ActiveRecord::Migration[4.2]
   def change
     create_table :shows do |t|
 
